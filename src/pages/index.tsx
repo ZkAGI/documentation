52e11929441f782f9a8b928b0bd7fee25cfdc1cf
Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>
2025-10-13 13:10:58 +0530

    feat: zynapse and branding

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,12 +33,12 @@ export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`ZkAGI Docs`}
+      description="Documentation">
       <HomepageHeader />
-      <main>
+      {/* <main>
         <HomepageFeatures />
-      </main>
+      </main> */}
     </Layout>
   );
 }
